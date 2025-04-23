0be3afc90944eee329584e6cfe2458836073820c
EOkten <eduardokten@gmail.com>
2025-04-23 21:22:02 +0300

    lesson 18 AsyncAPI, Spring Cloud Stream

--- src/main/java/ua/com/owu/sep2024/orderservice/event/ProductDeletedEventConsumer.java
+++ src/main/java/ua/com/owu/sep2024/orderservice/event/ProductDeletedEventConsumer.java
@@ -2,27 +2,23 @@ package ua.com.owu.sep2024.orderservice.event;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.listener.MessageListener;
 import org.springframework.stereotype.Component;
+import ua.com.owu.sep2024.orderservice.api.event.consumer.IOnProductDeletedEventConsumerService;
+import ua.com.owu.sep2024.orderservice.api.event.model.ProductDeletedEventPayload;
 import ua.com.owu.sep2024.orderservice.service.OrderService;
-import ua.com.owu.sep2024.productservice.event.ProductDeletedEvent;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class ProductDeletedEventConsumer implements MessageListener<Integer, ProductDeletedEvent> {
+public class ProductDeletedEventConsumer implements IOnProductDeletedEventConsumerService {
 
     private final OrderService orderService;
 
-    @KafkaListener(topics = "${topics.product-deleted}", groupId = "order-service-group")
     @Override
-    public void onMessage(ConsumerRecord<Integer, ProductDeletedEvent> data) {
-        ProductDeletedEvent event = data.value();
-        log.info("Received event: {}", event);
+    public void onProductDeletedEvent(ProductDeletedEventPayload payload, ProductDeletedEventPayloadHeaders headers) {
+        log.info("Received event: {}", payload);
 
-        orderService.getOrdersByProductId(event.productId()).forEach(order -> {
+        orderService.getOrdersByProductId(payload.getProductId()).forEach(order -> {
             orderService.updateOrderStatus(order.getId(), "CANCELLED");
             log.info("Order {} was cancelled as product in it had been deleted", order.getId());
         });
--- src/main/java/ua/com/owu/sep2024/productservice/event/ProductDeletedEvent.java
+++
@@ -1,6 +0,0 @@
-package ua.com.owu.sep2024.productservice.event;
-
-public record ProductDeletedEvent(
-        String productId
-) {
-}
